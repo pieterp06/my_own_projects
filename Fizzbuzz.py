a216eeff776a975215a18a726e5b33b8a1a6ae72
pieterp06 <pieterp06@gmail.com>
2025-06-17 17:54:32 +0900

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,15 @@
+
+for n in range (10):   #if dividable by 3 or 5 print Fizzbuzz
+
+    if n%3 == 0 and n%5 == 0:
+        print ("FizzBuzz")
+        n += 1
+ 
+    elif n%3 == 00:     #if dividable by 3 print Fizz
+        print ("Fizz")
+
+    elif n%5 == 00:     ##if dividable by 5 print Buzz
+        print ("Buzz")
+    
+    else: 
+        print (n)
